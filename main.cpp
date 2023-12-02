75813b2479d8fb6f5b97dba8bd963d5e946e95fe
Mansori <151128881+panda2077t@users.noreply.github.com>
2023-12-02 23:02:03 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+	
+	float a,b;
+	
+	cout<<   "first number is:";
+	
+	cin>> a;
+	
+	cout<<    "second number is:";
+	
+	cin>> b;
+	
+	cout<<     " a+b=  "  <<a+b;
+	
+	cout<<     " a-b=  "  <<a-b;
+	
+	cout<<     " a*b=   "  <<a*b;
+	
+	cout<<     " a / b=  " <<a  /    b;
+	
+	
+ 	
+ }
